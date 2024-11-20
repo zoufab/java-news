04d22260f30e2cb7e63e4bb056d723c67ca28f93
Zo <zo.rabarijaona@apside.fr>
2024-11-21 00:28:22 +0100

    📝 add java doc FilledRectangle

--- 21/twenty-one/src/main/java/com/zou/sealed_class/FilledRectangle.java
+++ 21/twenty-one/src/main/java/com/zou/sealed_class/FilledRectangle.java
@@ -1,3 +1,11 @@
 package com.zou.sealed_class;
 
+/**
+ * Represents a filled rectangle, which is a specific type of rectangle.
+ * This class extends the abstract {@link Rectangle} class and is marked as final.
+ * It does not add any new functionality, but serves as a concrete implementation of a rectangle.
+ *
+ * @author YourName
+ * @since 1.0
+ */
 public final class FilledRectangle extends Rectangle {  }
